5a6c19c7e99b1f2802e06895263533f88fef5af1
Andi Aziz <andesign.website@gmail.com>
2024-06-07 14:26:52 +0700

    Feat/social media layout (#2)
    * feat: update masthead, footer, lemon pop, video player
    
    * feat: update header, add button, update footer, update mobile view
    
    * feat: update viedeo asset
    
    * feat: sosmed wrapper
    
    * fix: improve animation
    
    * fix: convert snake_case
    
    * fix: convert to Image tag, decrease speed, pauseOnHover
    
    * fix: :zap: type issue on react-slick & next js image issue
    
    ---------
    
    Co-authored-by: Yosa Rama <yosamelody07@gmail.com>

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,19 +6,33 @@ interface ButtonProps {
   text: string
   href?: string
   type?: 'button' | 'submit' | 'reset'
+  newTab?: boolean
+  variant?: 'outline' | 'filled'
 }
 
-export default function Button({ text, href, type = 'button' }: ButtonProps) {
+export default function Button({
+  text,
+  href,
+  type = 'button',
+  variant,
+  newTab = false,
+}: ButtonProps) {
+  const buttonClass = variant === 'outline' ? s.outline_button : s.button
   if (href) {
     return (
-      <Link href={href} className={s.button}>
+      <Link
+        href={href}
+        className={buttonClass}
+        target={newTab ? '_blank' : '_self'}
+        rel={newTab ? 'noopener noreferrer' : ''}
+      >
         {text}
       </Link>
     )
   }
 
   return (
-    <button type={type} className={s.button}>
+    <button type={type} className={buttonClass}>
       {text}
     </button>
   )
--- src/contents/home/social-feed/index.tsx
+++ src/contents/home/social-feed/index.tsx
@@ -1,26 +1,100 @@
 'use client'
 
-import React from 'react'
+import Slider, { Settings as reactSlickSettings } from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import s from './index.module.scss'
+import Button from '@/components/button'
+import Image from 'next/image'
+interface SlideItemProps {
+  src: string
+  alt: string
+  url: string
+}
+
+const SlideItem = ({ src, alt, url }: SlideItemProps) => (
+  <a href={url} className={s.slide_item}>
+    <Image
+      className={s.slide_item_image}
+      src={src}
+      alt={alt}
+      width={420}
+      height={420}
+    />
+  </a>
+)
 
 export default function ContentsHomeSocialFeed() {
+  const imageSources = [
+    { src: '/assets/images/dummy/IG-1.jpg', url: '#' },
+    { src: '/assets/images/dummy/IG-2.jpg', url: '#' },
+    { src: '/assets/images/dummy/IG-6.jpg', url: '#' },
+    { src: '/assets/images/dummy/IG-7.jpg', url: '#' },
+  ]
+
+  let slickOptions: reactSlickSettings = {
+    centerMode: false,
+    centerPadding: '0px',
+    vertical: true,
+    infinite: true,
+    slidesToShow: 2.2,
+    slidesToScroll: 0.1,
+    autoplay: true,
+    arrows: false,
+    cssEase: 'linear',
+    pauseOnHover: true,
+    speed: 1000,
+    autoplaySpeed: 0,
+    responsive: [{ breakpoint: 768, settings: 'unslick' }],
+  }
+
   return (
     <section className={s.wrapper}>
       <div className={s.container}>
-        <div className={s.columnLeft}>
-          <h3 className={`mt-36 mb-14 ${s.heading}`}>
+        <div className={s.column_left}>
+          <h3
+            className={`mt-10 2xl:mt-36 mb-10 lg:mb-14 mx-auto lg:mx-0 ${s.heading}`}
+          >
             ADD FLAVOUR TO YOUR FEED
           </h3>
-          <p className={s.subheading}>
+          <p className={`mx-auto lg:mx-0 ${s.subheading}`}>
             Follow MINUS 196 on Instagram, Facebook, and TikTok for refreshing
             content that packs a punch.
           </p>
+
+          <div className={`mt-10 lg:mt-auto mb-10 ${s.social_media_link}`}>
+            <Button text="Instagram" href="#" variant="outline" newTab={true} />
+            <Button text="Facebook" href="#" variant="outline" newTab={true} />
+            <Button text="Tiktok" href="#" variant="outline" newTab={true} />
+          </div>
         </div>
-        <div className={s.columnRight}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
-          perferendis sint eligendi, saepe quidem eos nostrum corrupti aliquam
-          magni aut deleniti ipsum quam nobis earum debitis, eaque cumque
-          corporis consectetur.
+        <div className={s.column_right}>
+          <div className={`${s.vertical_images}`}>
+            <Slider {...slickOptions}>
+              {imageSources.map((image, index) => (
+                <SlideItem
+                  key={index}
+                  src={image.src}
+                  alt={`Vertical ${index + 1}`}
+                  url={image.url}
+                />
+              ))}
+            </Slider>
+          </div>
+          <div
+            className={`${s.vertical_images} ${s.second_block} hidden lg:block`}
+          >
+            <Slider {...slickOptions}>
+              {imageSources.map((image, index) => (
+                <SlideItem
+                  key={index}
+                  src={image.src}
+                  alt={`Vertical ${index + 1}`}
+                  url={image.url}
+                />
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
     </section>
