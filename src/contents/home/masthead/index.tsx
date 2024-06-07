25f588c765ad1c96bd5b508c5d36c48096f456c9
Andi Aziz <andesign.website@gmail.com>
2024-06-07 16:37:24 +0700

    feat: :art: integrate masthead with real image (#5)
    * feat: :art: integrate masthead with real image
    
    * fix: :zap: add Animate Presence on masthead
    
    ---------
    
    Co-authored-by: Yosa Rama <yosamelody07@gmail.com>

--- src/contents/home/masthead/index.tsx
+++ src/contents/home/masthead/index.tsx
@@ -5,7 +5,8 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import s from './index.module.scss'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
+import Image from 'next/image'
 
 export default function ContentsHomeMasthead() {
   const [activeSlide, setActiveSlide] = useState<number>(0)
@@ -19,7 +20,16 @@ export default function ContentsHomeMasthead() {
     autoplay: false,
     draggable: false,
     accessibility: true,
+    fade: true,
     beforeChange: (_current: number, next: number) => setActiveSlide(next),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   }
 
   const slideVariants = {
@@ -28,44 +38,204 @@ export default function ContentsHomeMasthead() {
     exit: { opacity: 0, scale: 0 },
   }
 
+  const textVariants = {
+    initial: { opacity: 0, x: 0 },
+    animate: { opacity: 1, x: -30 },
+    exit: { opacity: 0, x: 0 },
+  }
+  const text2Variants = {
+    initial: { opacity: 0, y: 0 },
+    animate: { opacity: 1, y: -30 },
+    exit: { opacity: 0, y: 0 },
+  }
+
   return (
-    <div className={s.masthead_carousel}>
-      <Slider {...settings}>
-        <div className={`overflow-hidden ${s.slick_item}`}>
-          <motion.div
-            initial="initial"
-            animate={activeSlide === 0 ? 'animate' : 'initial'}
-            variants={slideVariants}
-            transition={{
-              duration: 0.3,
-              delay: 0.8,
-              ease: [0.68, -0.55, 0.27, 1.55],
-            }}
-          >
-            Hello World
-            <img
-              src="/assets/images/can-1.png"
-              alt="slide 1"
-              width="170"
-              style={{ marginLeft: 'auto', marginRight: 'auto' }}
-            />
-          </motion.div>
-        </div>
-        <div className={s.slick_item}>
-          <motion.div
-            initial="initial"
-            animate={activeSlide === 1 ? 'animate' : 'initial'}
-            variants={slideVariants}
-            transition={{
-              duration: 0.3,
-              delay: 0.8,
-              ease: [0.68, -0.55, 0.27, 1.55],
-            }}
-          >
-            Hello World
-          </motion.div>
-        </div>
-      </Slider>
-    </div>
+    <AnimatePresence>
+      <div className={s.masthead_carousel}>
+        <Slider {...settings}>
+          <div className={`${s.slick_item} ${s.slick_1}`}>
+            <div className={s.main_content}>
+              <motion.div
+                initial="initial"
+                animate={activeSlide === 0 ? 'animate' : 'initial'}
+                variants={slideVariants}
+                transition={{
+                  duration: 0.3,
+                  delay: 0.6,
+                  ease: [0.68, -0.55, 0.27, 1.55],
+                }}
+                className={s.can_1}
+              >
+                <Image
+                  width={566}
+                  height={875}
+                  alt="Grape"
+                  src={'/assets/images/Home_Masthead-1_Grape.png'}
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </motion.div>
+              <motion.div
+                initial="initial"
+                animate={activeSlide === 0 ? 'animate' : 'initial'}
+                variants={slideVariants}
+                transition={{
+                  duration: 0.3,
+                  delay: 0.9,
+                  ease: [0.68, -0.55, 0.27, 1.55],
+                }}
+                className={s.can_2}
+              >
+                <Image
+                  width={566}
+                  height={875}
+                  alt="Lemon"
+                  src={'/assets/images/Home_Masthead-1_Lemon.png'}
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </motion.div>
+              <motion.div
+                initial="initial"
+                animate={activeSlide === 0 ? 'animate' : 'initial'}
+                variants={textVariants}
+                transition={{ duration: 0.5, delay: 1.3, ease: 'easeOut' }}
+                className={s.text_1}
+              >
+                <Image
+                  src="/assets/images/Home_Masthead-1_NO1-premix_masthead.svg"
+                  width={480}
+                  height={350}
+                  alt="No.1 Premix"
+                  style={{
+                    objectFit: 'contain',
+                  }}
+                />
+              </motion.div>
+            </div>
+            <motion.div
+              initial="initial"
+              animate={activeSlide === 0 ? 'animate' : 'initial'}
+              variants={text2Variants}
+              transition={{ duration: 0.5, delay: 1.8, ease: 'easeOut' }}
+              className={s.text_2}
+            >
+              <Image
+                src="/assets/images/Home_Masthead-1_real-fruit-taste.svg"
+                width={203}
+                height={75}
+                alt="Real Fruit Taste"
+                style={{
+                  objectFit: 'contain',
+                }}
+              />
+            </motion.div>
+          </div>
+          <div className={`${s.slick_item} ${s.slick_2}`}>
+            <div className={s.main_content}>
+              <motion.div
+                initial="initial"
+                animate={activeSlide === 1 ? 'animate' : 'initial'}
+                variants={slideVariants}
+                transition={{
+                  duration: 0.3,
+                  delay: 0.6,
+                  ease: [0.68, -0.55, 0.27, 1.55],
+                }}
+                className={s.can_1}
+              >
+                <Image
+                  width={566}
+                  height={875}
+                  alt="Grape"
+                  src={'/assets/images/Home_Masthead-2_Grapefruit.png'}
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </motion.div>
+              <motion.div
+                initial="initial"
+                animate={activeSlide === 1 ? 'animate' : 'initial'}
+                variants={slideVariants}
+                transition={{
+                  duration: 0.3,
+                  delay: 0.9,
+                  ease: [0.68, -0.55, 0.27, 1.55],
+                }}
+                className={s.can_2}
+              >
+                <Image
+                  width={566}
+                  height={875}
+                  alt="Shekwasha"
+                  src={'/assets/images/Home_Masthead-2_Shekwasha.png'}
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </motion.div>
+              <motion.div
+                initial="initial"
+                animate={activeSlide === 1 ? 'animate' : 'initial'}
+                variants={slideVariants}
+                transition={{
+                  duration: 0.3,
+                  delay: 1.2,
+                  ease: [0.68, -0.55, 0.27, 1.55],
+                }}
+                className={s.can_3}
+              >
+                <Image
+                  width={566}
+                  height={875}
+                  alt="Lemon"
+                  src={'/assets/images/Home_Masthead-2_Lemon.png'}
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </motion.div>
+              <motion.div
+                initial="initial"
+                animate={activeSlide === 1 ? 'animate' : 'initial'}
+                variants={textVariants}
+                transition={{ duration: 0.5, delay: 1.5, ease: 'easeOut' }}
+                className={s.text_1}
+              >
+                <Image
+                  src="/assets/images/9percent_masthead_right_align_1.svg"
+                  width={537}
+                  height={380}
+                  alt="9% Alcohol"
+                  style={{
+                    objectFit: 'contain',
+                  }}
+                />
+              </motion.div>
+            </div>
+            <motion.div
+              initial="initial"
+              animate={activeSlide === 1 ? 'animate' : 'initial'}
+              variants={text2Variants}
+              transition={{ duration: 0.5, delay: 2.0, ease: 'easeOut' }}
+              className={s.text_2}
+            >
+              <Image
+                src="/assets/images/Home_Masthead-1_real-fruit-taste.svg"
+                width={203}
+                height={75}
+                alt="Real Fruit Taste"
+                style={{
+                  objectFit: 'contain',
+                }}
+              />
+            </motion.div>
+          </div>
+        </Slider>
+      </div>
+    </AnimatePresence>
   )
 }
